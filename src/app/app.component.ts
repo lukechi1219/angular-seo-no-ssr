89104c4eff260cf840167a99d4d57a75f60d589a
luke.chi <luke@neutec.com.tw>
2021-12-13 17:03:01 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-seo-no-ssr';
+
+  constructor(private metaTagService: Meta) {
+  }
+
+  ngOnInit() {
+
+    // this.metaTagService.updateTag({ charset: 'UTF-8' },);
+
+    this.metaTagService.addTags([
+      { name: 'author', content: 'luke.chi@gmail.com' },
+      // { name: 'keywords', content: 'Angular SEO Integration, Music CRUD, Angular Universal' },
+      // { name: 'robots', content: 'index, follow' },
+      // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      // { name: 'date', content: '2019-10-31', scheme: 'YYYY-MM-DD' },
+    ]);
+  }
 }
