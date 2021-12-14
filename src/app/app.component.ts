0f20cd35ac46645c2ce831cf906f77509bd92e39
luke.chi <luke@neutec.com.tw>
2021-12-14 19:00:24 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Meta } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,21 +7,28 @@ import { Meta } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-seo-no-ssr';
+  project = 'angular-seo-no-ssr';
+  title = 'How to do SEO without Angular Universal Server-Side Rendering?';
 
-  constructor(private metaTagService: Meta) {
+  constructor(private titleService: Title, private metaTagService: Meta) {
   }
 
   ngOnInit() {
+
+    this.titleService.setTitle(this.title);
 
     // this.metaTagService.updateTag({ charset: 'UTF-8' },);
 
     this.metaTagService.addTags([
       { name: 'author', content: 'luke.chi@gmail.com' },
-      // { name: 'keywords', content: 'Angular SEO Integration, Music CRUD, Angular Universal' },
+      {
+        name: 'description',
+        content: 'Did you know that Google Search supports JavaScript since May 2019? ' +
+          'This site shows you Angular features for SEO without Angular Universal.'
+      },
       // { name: 'robots', content: 'index, follow' },
-      // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       // { name: 'date', content: '2019-10-31', scheme: 'YYYY-MM-DD' },
+      // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ]);
   }
 }
